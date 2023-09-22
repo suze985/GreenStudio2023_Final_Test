16cd6dc30be21ecea47cb7148786dd7a337fa620
suze985 <145733956+suze985@users.noreply.github.com>
2023-09-22 22:32:29 +0800

    Add files via upload

---
+++ 智能2302赵苏泽/水仙花数和查找数组元素/水仙花数.cpp
@@ -0,0 +1,25 @@
+//#include"tou.h"
+//
+//int main()
+//{
+//	cout << "1-1000的水仙花数有 ：" << endl;
+//	for (int num = 100; num < 1000; num++)//水仙花数首先是个三位数
+//	{
+//		int a = 0;
+//		int b = 0;
+//		int c = 0;
+//
+//
+//		a = num / 100;
+//		b = num / 10 % 10;	
+//		c = num % 10;
+//
+//		if (a * a * a + b * b * b + c * c * c == num)
+//		{
+//			cout  << num << endl;
+//		}
+//
+//	}
+//	system("pause");
+//	return 0;
+//}
